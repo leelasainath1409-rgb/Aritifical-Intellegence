eaf0850a438b0993bb4dec784f812670f7f5dd40
leelasainath1409-rgb <leelasainath1409@gmail.com>
2026-05-18 19:23:50 +0530

    Enhance sentiment analysis app with file upload feature

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ import streamlit as st
 import pickle
 import re
 import nltk
+import pandas as pd
 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
 nltk.download('stopwords')
 
+# LOAD MODEL
 model = pickle.load(open('sentiment_model.pkl', 'rb'))
 tfidf = pickle.load(open('tfidf_vectorizer.pkl', 'rb'))
 
 stemmer = PorterStemmer()
 
+# CLEAN FUNCTION
 def clean_text(text):
 
     text = text.lower()
@@ -29,23 +32,62 @@ def clean_text(text):
 
     return " ".join(words)
 
-st.title("NLP Sentiment Analysis")
+# TITLE
+st.title("NLP SENTIMENT ANALYSIS SYSTEM")
 
-review = st.text_area("Enter Review")
+# OPTION MENU
+option = st.selectbox(
+    "Choose Option",
+    ("Input Text", "Upload File")
+)
 
-if st.button("Predict"):
+# INPUT TEXT OPTION
+if option == "Input Text":
 
-    cleaned_review = clean_text(review)
+    review = st.text_area("Enter Review")
 
-    vector_input = tfidf.transform([cleaned_review]).toarray()
+    if st.button("Predict"):
 
-    prediction = model.predict(vector_input)
+        cleaned_review = clean_text(review)
 
-    if prediction[0] == 2:
-        st.success("Positive Sentiment")
+        vector_input = tfidf.transform([cleaned_review]).toarray()
 
-    elif prediction[0] == 1:
-        st.warning("Neutral Sentiment")
+        prediction = model.predict(vector_input)
 
-    else:
-        st.error("Negative Sentiment")
+        if prediction[0] == 2:
+            st.success("Positive Sentiment")
+
+        elif prediction[0] == 1:
+            st.warning("Neutral Sentiment")
+
+        else:
+            st.error("Negative Sentiment")
+
+# FILE UPLOAD OPTION
+elif option == "Upload File":
+
+    uploaded_file = st.file_uploader(
+        "Upload CSV File",
+        type=["csv"]
+    )
+
+    if uploaded_file is not None:
+
+        data = pd.read_csv(uploaded_file)
+
+        st.write(data.head())
+
+        if 'review' in data.columns:
+
+            clean_reviews = data['review'].apply(clean_text)
+
+            vectors = tfidf.transform(clean_reviews).toarray()
+
+            predictions = model.predict(vectors)
+
+            data['Prediction'] = predictions
+
+            st.write(data)
+
+        else:
+            st.error("CSV must contain 'review' column")
